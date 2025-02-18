aed01b9457b0d828fd9523ccdb862a8012e7372c
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-02-18 22:52:52 +0500

    Key Features

---
+++ src/Components/KeyFeatures/KeyFeatures.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function KeyFeatures(){
+
+
+
+    return(
+        <section className="key-features bg-[#1a3550] w-full ">
+
+
+        </section>
+    )
+}
